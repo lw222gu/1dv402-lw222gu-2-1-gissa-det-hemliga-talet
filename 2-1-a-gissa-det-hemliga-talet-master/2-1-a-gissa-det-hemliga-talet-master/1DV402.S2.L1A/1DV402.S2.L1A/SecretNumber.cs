2b59d4c7f54c8f50baab38bf7bf7d3654261cd8c
Lisa Westlund <lw222gu@student.lnu.se>
2014-10-11 22:33:06 +0200

    Klar med konstruktor och metoder

--- 2-1-a-gissa-det-hemliga-talet-master/2-1-a-gissa-det-hemliga-talet-master/1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 2-1-a-gissa-det-hemliga-talet-master/2-1-a-gissa-det-hemliga-talet-master/1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -6,50 +6,113 @@ using System.Threading.Tasks;
 
 namespace _1DV402.S2.L1A
 {
-    class SecretNumber
+    public class SecretNumber
     {
         private int _count;
         private int _number;
-        public const int _MaxNumberOfGuesses = 7;
+        public const int MaxNumberOfGuesses = 7;
 
         public int Count
         {
-            get {return _count;}
-            set
-            {
-            }
+            get { return _count; }
+            set { _count = value; }
         }
 
         public int Number
         {
-            get {return _number;}
-            set 
-            {
-            }
+            get { return _number; }
+            set { _number = value; }
         }
-        
+
+
         public SecretNumber()
         {
-        }
-        
-        public SecretNumber( int count, int number, int MaxNumberOfGuesses )
-        {
-            Count = count;
-            Number = number;
+            Count = _count;
+            Number = _number;
             Initialize();
         }
 
+        //public SecretNumber(int count, int number, int MaxNumberOfGuesses)
+        //{
+        //    Count = count;
+        //    Number = number;
+        //    Initialize();
+        //}
+
         public void Initialize()
         {
-            Random _number = new Random();
-            int secretNumber = _number.Next(1, 101);
+            Random rnd = new Random();
+            _number = rnd.Next(1, 100);
             _count = 0;
         }
 
         public bool MakeGuess(int number)
         {
 
+            if (_count == MaxNumberOfGuesses)
+            { 
+                throw new ApplicationException(); 
+            }
+
+            _count++;
+
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (number < Number)
+            {
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - Count));
+                return false;
+            }
+
+            if (number > Number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - Count));
+                return false;
+            }
+
+            if (number == Number)
+            {
+                Console.WriteLine("Rätt gissat! Du använde {0} gissningar", Count);
+                return true;
+            }
+
+            return true;
         }
+
+
+    
+
+
+
+
+        //public bool MakeGuess(int number);
+        //{            
+        //    if (Count == MaxNumberOfGuesses)
+        //    {
+        //        Console.WriteLine("Du har inga gissningar kvar, det hemliga talet var {0}", Number);
+        //        return true;
+        //    }
+
+        //    if (Count != MaxNumberOfGuesses)
+        //    {
+        //        Count++;
+        //    }
+
+        //    if (number == Number)
+        //    {
+        //        Console.WriteLine("Du gissade rätt! Det hemliga talet är {0}. Du använde {1} antal gissningar.", Number, Count);
+        //        count++;
+        //        return true;
+        //    }
+
+        //    if (1 > number || number > 100)
+        //    {
+        //        throw new ArgumentOutOfRangeException();
+        //    }
+        //}
 
         //public int Count ()
         //{
@@ -62,32 +125,7 @@ namespace _1DV402.S2.L1A
         //    set {_number = value;}
         //}
 
+    }
  
-
-        //public bool MakeGuess(int number);
-        //{            
-        //    if (Count == MaxNumberOfGuesses)
-        //    {
-        //        Console.WriteLine("Du har inga gissningar kvar, det hemliga talet var {0}", Number);
-        //        return true;
-        //    }
-    
-        //    if (Count != MaxNumberOfGuesses)
-        //    {
-        //        Count++;
-        //    }
-
-        //    if (number == Number)
-        //    {
-        //        Console.WriteLine("Du gissade rätt! Det hemliga talet är {0}. Du använde {1} antal gissningar.", Number, Count);
-        //        count++;
-        //        return true;
-        //    }
-        
-        //    if (1 > number || number > 100)
-        //    {
-        //        throw new ArgumentOutOfRangeException();
-        //    }
-        //}
 
 }
